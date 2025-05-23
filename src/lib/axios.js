030e9fb3cddc62a23e2a01310008abf4b7626222
revarao31 <revarao31@gmail.com>
2025-05-23 17:25:50 +0530

    Added Files

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,9 @@
+// src/lib/axios.js
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_BASE_URL || "https://chatbuddy-uy11.onrender.com/apii";
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL,
   withCredentials: true,
 });
